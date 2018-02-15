3098f4ccb0967fcc9b70b13e8523119b91802e9b
Paul Lindberg <paul.lindberg@mymail.champlain.edu>
2018-02-15 13:59:00 -0500

    even more editing
    now it actually gets question 1 correct!!
    xdxdxd xd XD

--- udp_client.py
+++ udp_client.py
@@ -17,11 +17,18 @@ import random
 DEFAULT = uuid.uuid4()
 
 
+# Time out exception
+
 class TimeOutError(Exception):
     pass
 
 
+# one line summary
+
 class UDPClient:
+
+    # class initializer
+
     def __init__(self, host, port, temp_bool=DEFAULT):
         if temp_bool is DEFAULT:
             self.request_id = -1
@@ -35,34 +42,58 @@ class UDPClient:
         except:
             raise TimeOutError('Failed to set a socket')
 
+    # one line summary
+
     def send_message_by_character(self, message):
         if self.request_id >= 0:
             print("AAAAA ID")
             # message = self.request_id + "|" + message
             # print('Server says {!r}'.format(data.decode('ascii')))
-        else:
-            temp_delay = constants.INITIAL_TIMEOUT
-            # data = message.encode('ascii')
             message_list_send = list(message)
             message_len = len(message_list_send)
             message_list_receive = []
             for i in range(0, message_len):
+                temp_delay = constants.INITIAL_TIMEOUT
                 while True:
                     data = message_list_send[i].encode('ascii')
                     self.sock.send(data)
-                    print('Waiting up to {} seconds for a reply'.format(constants.INITIAL_TIMEOUT))
-                    self.sock.settimeout(constants.INITIAL_TIMEOUT)
+                    print('Waiting up to {} seconds for a reply'.format(temp_delay))
+                    self.sock.settimeout(temp_delay)
                     try:
                         data = self.sock.recv(constants.MAX_BYTES)
                     except socket.timeout as exc:
                         temp_delay *= 2  # wait even longer for the next request
-                        # if temp_delay > 2.0:
-                        #     raise TimeOutError('I think the server is down') from exc
+                        if temp_delay > 2.0:
+                            raise TimeOutError('I think the server is down') from exc
                     else:
                         break  # we are done, and can stop looping
                 message_list_receive.append(data.decode('ascii'))
             message_str_receive = "".join(message_list_receive)
             print('Server says {!r}'.format(message_str_receive))
+            return message_str_receive
+        else:
+            message_list_send = list(message)
+            message_len = len(message_list_send)
+            message_list_receive = []
+            for i in range(0, message_len):
+                temp_delay = constants.INITIAL_TIMEOUT
+                while True:
+                    data = message_list_send[i].encode('ascii')
+                    self.sock.send(data)
+                    print('Waiting up to {} seconds for a reply'.format(temp_delay))
+                    self.sock.settimeout(temp_delay)
+                    try:
+                        data = self.sock.recv(constants.MAX_BYTES)
+                    except socket.timeout as exc:
+                        temp_delay *= 2  # wait even longer for the next request
+                        if temp_delay > 2.0:
+                            raise TimeOutError('I think the server is down') from exc
+                    else:
+                        break  # we are done, and can stop looping
+                message_list_receive.append(data.decode('ascii'))
+            message_str_receive = "".join(message_list_receive)
+            print('Server says {!r}'.format(message_str_receive))
+            return message_str_receive
 
 
 if __name__ == "__main__":
